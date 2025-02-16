8d6fe47e76aa8d7411b9cf072124e297520a72d4
TheScarecr0w <125376893+TheScarecr0w@users.noreply.github.com>
2025-02-16 11:21:30 +0300

    Delete log.py

--- log.py
+++
@@ -1,8 +0,0 @@
-import time
-from app import VoiceLib
-
-time_log = (time.strftime('%H:%M:%S', time.localtime()))
-
-f = open('logs.txt', 'w')
-f.write(str(time_log) + ': ' + VoiceLib + '\n')
-f.close()
